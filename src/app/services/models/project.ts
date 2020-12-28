d5010a6ceb3ffe8bca69ec58edbe3abab509fc41
William Karavites <will@willkara.com>
2020-12-28 15:05:23 -0500

    Initial API Service Setup

---
+++ src/app/services/models/project.ts
@@ -0,0 +1,7 @@
+export interface Project {
+  projectName: string;
+  projectLink: string;
+  projectImage: string;
+  projectActive: boolean;
+  projectDescription: string;
+}
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import API from '@aws-amplify/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectService {
+  apiName = 'projectListAPI';
+  path = '/projects';
+
   constructor() {}
+
+  getProjects(): void {
+    console.log('qqqqqqqqqqq');
+  }
 }
